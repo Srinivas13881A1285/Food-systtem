b7fae6686e1c5901caa336bfab7c71037cb12230
Srinivas Chintakindhi <srinusreenivas552@gmail.com>
2021-06-29 22:14:41 +0530

    Add files via upload

---
+++ BusinessEmployee.java
@@ -0,0 +1,20 @@
+package mini2;
+
+public class BusinessEmployee extends Employee {
+
+    public BusinessEmployee(String name){
+        super(name,50000);
+    }
+
+    public double getBonusBudget(){
+        return bonusBudget;
+    }
+
+    public void setBonusBudget(double bonusBudget){
+        this.bonusBudget = bonusBudget;
+    }
+
+    public String employeeStatus(){
+        return this.toString() + " with a budget of " + bonusBudget;
+    }
+}
---
+++ TechnicalEmployee.java
@@ -0,0 +1,19 @@
+package mini2;
+
+public class TechnicalEmployee extends Employee {
+
+    public int checkin;
+
+    public TechnicalEmployee(String name){
+        super(name, 75000);
+        checkin = 0;
+    }
+
+    public String employeeStatus() {
+        return super.toString() + " has " + checkin + " successful checkins";
+    }
+
+    public void setCheckin(){
+        checkin++;
+    }
+}
